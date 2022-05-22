737505d692fdb7aae5c44402f47795abbced305a
Mario Bezerra <mariomgsb@gmail.com>
2022-05-22 12:29:47 -0300

    Adicionando o metodo de remover topico

--- src/main/java/br/com/escola/Forum/controller/TopicosController.java
+++ src/main/java/br/com/escola/Forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,6 +44,7 @@ public class TopicosController {
 	}
 	
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 	
 		Topico topico = form.toTopico(cursoRepository);
@@ -67,5 +69,13 @@ public class TopicosController {
 		Topico topico = form.atualizar(topicoRepository, id);
 		
 		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable("id") Long id){
+		
+		topicoRepository.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 }
