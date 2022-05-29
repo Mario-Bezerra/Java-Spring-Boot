4ed03ab69e87e644c464eaf459aeb09716256cf3
Mario Bezerra <mariomgsb@gmail.com>
2022-05-29 14:51:02 -0300

    Corrigindo classes de testes para os mais recentes, e adicionando arquivo de imagem Docker

--- src/test/java/br/com/escola/Forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/escola/Forum/repository/CursoRepositoryTest.java
@@ -1,13 +1,15 @@
 package br.com.escola.Forum.repository;
 
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import br.com.escola.Forum.modelo.Curso;
 
 @SpringBootTest
+@ActiveProfiles("prod")
 class CursoRepositoryTest {
 
 	@Autowired
@@ -17,15 +19,15 @@ class CursoRepositoryTest {
 	public void cursoDeveriaSerEncontradoComNome() {
 		String nomeCurso = "Spring Boot";
 		Curso curso = cursoRepository.findByNome(nomeCurso);
-		Assert.assertNotNull(curso);
-		Assert.assertEquals(nomeCurso, curso.getNome());
+		Assertions.assertNotNull(curso);
+		Assertions.assertEquals(nomeCurso, curso.getNome());
 	}
 	
 	@Test
 	public void cursoNãoDeveriaSerEncontradoQuandoPassadoNomeErrado() {
 		String nomeCurso = "Zacarias Boot";
 		Curso curso = cursoRepository.findByNome(nomeCurso);
-		Assert.assertNull(curso);
+		Assertions.assertNull(curso);
 	}
 
 }
